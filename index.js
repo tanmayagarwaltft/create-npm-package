85e6e4c40f127ce9e5e9011aa91c5d789e48e7cb
Tanmay-admin <tanmay-admin@TanmayA-MacBookAir.local>
2024-07-03 18:56:45 +0530

    second commit

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_Op1pla6au1yWj71uAFY7voYQLtkwph5CqgGK7ui9');
 
+// convert from currency to currency units
 export async function convertCurrency(fromCurrency, toCurrency, units){
    const response =  await freecurrencyapi.latest({
         base_currency: fromCurrency,
         currencies: toCurrency
     });
     const multiplier = response.data[toCurrency];
+
     return multiplier*units;
 }
 
